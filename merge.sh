ede6ed37e62ccdc3ca699125d29b652ebe833f89
Brian Broll <brian.broll@vanderbilt.edu>
2015-02-05 16:30:36 -0600

    fixing travis

--- merge.sh
+++ merge.sh
@@ -11,6 +11,7 @@ echo "current branch is '$CURRENT_BRANCH'"
 if [ "$CURRENT_BRANCH" = "$FROM_BRANCH" ] ; then
     git checkout $FROM_BRANCH && \
     echo "Checking out $TO_BRANCH..." && \
+    git pull origin $TO_BRANCH && \
     git checkout $TO_BRANCH && \
     echo "Merging changes..." && \
     git merge $FROM_BRANCH && \
