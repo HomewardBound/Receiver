a73719376576cb01013b1fb751450e4d494b2d39
Brian Broll <brian.broll@vanderbilt.edu>
2015-02-07 11:44:48 -0600

    Added basic test list and removed merge script

--- test/test.js
+++ test/test.js
@@ -1,7 +1,28 @@
-/*globals describe, it*/
+/*globals describe,it,assert,before*/
 
 'use strict';
-describe('Testing npm test', function() {
-    it('should work', function() {
+var assert = require('assert'),
+    Message = require('../lib/message');
+
+describe('Testing Receiver', function() {
+    before(function() {
+        // Create the receiver object
+    });
+
+    it('should pass incoming measurements to computation apps', function() {
+        assert(false, 'Need to write this test');
+    });
+
+    // Check if it is a pet
+    it('should verify that the measurement is a valid entity', function() {
+        assert(false, 'Need to write this test');
+    });
+
+    it('should store measurement from computation apps', function() {
+        assert(false, 'Need to write this test');
+    });
+
+    it('should submit message to client apps on storage of measurement', function() {
+        assert(false, 'Need to write this test');
     });
 });
